8387ef5a49d9a11500936a2b4b35175d88c1bede
Timo Weberruß <timo.weberruss@student.kit.edu>
2024-12-19 17:55:35 +0100

    Initial commit

---
+++ src/main/java/com/tikelespike/gamestats/GamestatsApplication.java
@@ -0,0 +1,13 @@
+package com.tikelespike.gamestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamestatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamestatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tikelespike/gamestats/GamestatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikelespike.gamestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
