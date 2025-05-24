b9e1d6eb22e4dda173404a6b8af0b6a76309b570
Timo Weberruß <timo.weberruss@student.kit.edu>
2025-05-24 23:30:55 +0200

    Add initial admin user

--- src/main/java/com/tikelespike/gamestats/GamestatsApplication.java
+++ src/main/java/com/tikelespike/gamestats/GamestatsApplication.java
@@ -1,7 +1,13 @@
 package com.tikelespike.gamestats;
 
+import com.tikelespike.gamestats.businesslogic.entities.UserCreationRequest;
+import com.tikelespike.gamestats.businesslogic.entities.UserRole;
+import com.tikelespike.gamestats.businesslogic.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 /**
@@ -22,6 +28,30 @@ public class GamestatsApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(GamestatsApplication.class, args);
+    }
+
+    /**
+     * Upon application startup, check if an admin user exists, and if not, create one with a default password.
+     *
+     * @param userService the user service to use for checking and creating the admin user
+     * @param encoder the password encoder to use for encoding the admin password
+     *
+     * @return a CommandLineRunner that will be executed on application startup
+     */
+    @Bean
+    CommandLineRunner initAdmin(UserService userService, PasswordEncoder encoder) {
+        return args -> {
+            if (userService.loadUserByUsername("admin") == null) {
+                UserCreationRequest admin = new UserCreationRequest(
+                        "System Administrator",
+                        "admin",
+                        encoder.encode("admin"),
+                        UserRole.ADMIN,
+                        null
+                );
+                userService.createUser(admin);
+            }
+        };
     }
 
 }
