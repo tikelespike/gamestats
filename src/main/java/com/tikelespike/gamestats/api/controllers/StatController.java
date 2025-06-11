297f3c9c51d4f8f3bf8d4b253d2706051bf135f4
Timo Weberruß <timo.weberruss@student.kit.edu>
2025-06-12 00:09:54 +0200

    Attach service to REST controller

--- src/main/java/com/tikelespike/gamestats/api/controllers/StatController.java
+++ src/main/java/com/tikelespike/gamestats/api/controllers/StatController.java
@@ -2,6 +2,9 @@ package com.tikelespike.gamestats.api.controllers;
 
 import com.tikelespike.gamestats.api.entities.ErrorEntity;
 import com.tikelespike.gamestats.api.entities.PlayerStatsDTO;
+import com.tikelespike.gamestats.api.mapper.PlayerStatsMapper;
+import com.tikelespike.gamestats.businesslogic.entities.PlayerStats;
+import com.tikelespike.gamestats.businesslogic.services.StatService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * REST controller providing different statistics about the playing group.
  */
@@ -24,6 +29,21 @@ import org.springframework.web.bind.annotation.RestController;
         description = "Retrieve statistics about the playing group (like leaderboards)"
 )
 public class StatController {
+    private final StatService statService;
+    private final PlayerStatsMapper playerStatsMapper;
+
+    /**
+     * Creates a new StatController. This is usually done by the Spring framework, which manages the controller's
+     * lifecycle and injects the required dependencies.
+     *
+     * @param statService service providing statistical data
+     * @param playerStatsMapper mapper for converting player statistics to transfer objects
+     */
+    public StatController(StatService statService, PlayerStatsMapper playerStatsMapper) {
+        this.statService = statService;
+        this.playerStatsMapper = playerStatsMapper;
+    }
+
     /**
      * Retrieves statistics about each player, such as won games or amount of times the player played as part of the
      * evil team.
@@ -64,7 +84,10 @@ public class StatController {
     )
     @GetMapping("/players")
     public ResponseEntity<Object> getAllPlayerStatistics() {
-        return ResponseEntity.internalServerError()
-                .body("This endpoint is not yet implemented. Please check back later.");
+        List<PlayerStats> stats = statService.getAllPlayerStatistics();
+        List<PlayerStatsDTO> statsDTO = stats.stream()
+                .map(playerStatsMapper::toTransferObject)
+                .toList();
+        return ResponseEntity.ok(statsDTO);
     }
 }
