eec5568e07d525ec2b740855c56c0c547f6549a2
Timo Weberruß <timo.weberruss@student.kit.edu>
2026-01-02 01:00:36 +0100

    Fix unrecognized properties in JSON

--- src/main/java/com/tikelespike/gamestats/gateways/scripttool/OfficialCharacterDTO.java
+++ src/main/java/com/tikelespike/gamestats/gateways/scripttool/OfficialCharacterDTO.java
@@ -1,6 +1,8 @@
 package com.tikelespike.gamestats.gateways.scripttool;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Reverse-engineered format of an official character (also known as role) as used in the official script tool.
  *
@@ -9,23 +11,24 @@ package com.tikelespike.gamestats.gateways.scripttool;
  * @param team character type (e.g. townsfolk)
  * @param edition which set the character was released as part of
  * @param firstNightReminder first night storyteller reminder text
- * @param reminders other nights storyteller reminder texts
+ * @param otherNightReminder other nights storyteller reminder text
+ * @param reminders reminder tokens belonging to this character
  * @param setup whether the character requires special setup before characters are assigned to the players
  * @param ability description of the character ability
  * @param flavor flavor text for the character
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 record OfficialCharacterDTO(
         String id,
         String name,
         String team,
         String edition,
         String firstNightReminder,
+        String otherNightReminder,
         String[] reminders,
         boolean setup,
         String ability,
         String flavor
 ) {
-    boolean isGood() {
-        return team.equals("townsfolk") || team.equals("outsider");
-    }
+
 }
